d83405f7585ae413664538a2bde9af64e481bec6
Victor <vmagalhaes@id.uff.br>
2021-11-10 18:10:10 -0300

    :circus_tent: funcoes de clima atualizadas

--- skill/clima.py
+++ skill/clima.py
@@ -17,3 +17,33 @@ def climatologia(ds: xr.Dataset, start: str, end: str) -> xr.Dataset:
     monthly_clim = ds_sliced.groupby('time.month').mean('time')
     
     return monthly_clim
+
+
+def anomalia(obj: xr.Dataset, clima: xr.Dataset) -> xr.Dataset:
+    """
+    Retorna a anomalia de acordo com as entradas.
+    
+    obj: xr.Dataset
+        Dataset com os dados em análise
+    clima: xr.Dataset
+        Dataset com a climatologia desejada.
+    """
+    
+    anomalia = obj.groupby('time.month') - clima.groupby('time.month')
+    
+    return anomalia
+
+
+def correlacao(prev: xr.Dataset, obs: xr.Dataset) -> xr.Dataset:
+    """
+    Faz a correlação de pearson entre o dado previsto e o observado.
+    
+    prev: xr.Dataset
+        Dataset com os dados de previsão
+    obs: xr.Dataset
+        Dataset com os dados observados.
+    """
+    
+    corr = xr.corr(prev, obs)
+    
+    return corr
