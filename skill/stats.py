579214aaaf17bd13f2e195a0e24dae25d2e8f57f
Victor <vmagalhaes@id.uff.br>
2021-11-22 13:38:53 -0300

    :tada: funcao para calculo do rmse

--- skill/stats.py
+++ skill/stats.py
@@ -1,15 +1,30 @@
 import xarray as xr
+import xskillscore as xs
 
-def correlacao(prev: xr.Dataset, obs: xr.Dataset) -> xr.Dataset:
+def correlacao(prev: xr.DataArray, obs: xr.DataArray) -> xr.DataArray:
     """
     Faz a correlação de pearson entre o dado previsto e o observado.
     
-    prev: xr.Dataset
+    prev: xr.DataArray
         Dataset com os dados de previsão
-    obs: xr.Dataset
+    obs: xr.DataArray
         Dataset com os dados observados.
     """
     
     corr = xr.corr(prev, obs, dim='time')
     
     return corr
+
+def rmse(prev:xr.DataArray, obs:xr.DataArray) -> xr.DataArray:
+    """
+    Calcula a RMSE entre o dado previsto e o observado.
+    
+    prev: xr.DataArray
+        Dados previstos
+    obs: xr.DataArray
+        Dados observados
+    """
+    
+    rmse = xs.rmse(prev, obs, dim='time')
+    
+    return rmse
