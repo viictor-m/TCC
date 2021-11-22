31f0e2d578038b0ef5bbd1096d2f28497529d7f9
Victor <vmagalhaes@id.uff.br>
2021-11-22 11:09:44 -0300

    trocando funcao correlacao de clima para stats

--- skill/clima.py
+++ skill/clima.py
@@ -33,18 +33,3 @@ def anomalia(obj: xr.Dataset, clima: xr.Dataset) -> xr.Dataset:
     anomalia = obj.groupby('time.month') - clima
     
     return anomalia
-
-
-def correlacao(prev: xr.Dataset, obs: xr.Dataset) -> xr.Dataset:
-    """
-    Faz a correlação de pearson entre o dado previsto e o observado.
-    
-    prev: xr.Dataset
-        Dataset com os dados de previsão
-    obs: xr.Dataset
-        Dataset com os dados observados.
-    """
-    
-    corr = xr.corr(prev, obs, dim='time')
-    
-    return corr
